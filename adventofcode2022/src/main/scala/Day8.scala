f4bb68e128dc1c2c053e18770a8f93a1058d0559
Mihhail Novik <mihhail.novik@gmail.com>
2022-12-08 17:36:34 +0700

    Advent of Code 2022. Day 8. Skeleton

--- adventofcode2022/src/main/scala/Day8.scala
+++ adventofcode2022/src/main/scala/Day8.scala
@@ -4,23 +4,68 @@ import scala.collection.mutable.Map
 class Day8 extends AoCPuzzle {
   override def dayNr: Int = 8
 
-  override def active(): Boolean = false
+  override def active(): Boolean = true
 
-  val input = getData().map(_.toCharArray.map(_.toString.toInt)).toArray
+  private val input = getData().map(_.toCharArray.map(_.toString.toInt)).toArray
+  private val forrest = Forrest(input)
 
-  case class Forrest(data: Array[Array[Int]]) {
+  private def countVisibleTrees(treeHeight: Int, coordinate: Coordinate, direction: Direction, forrest: Forrest, acc: Int): Int = {
+    if (forrest.isEdge(coordinate)) acc else {
+      val newCoordinate = coordinate.move(direction)
+      val sideTree = forrest.value(newCoordinate)
+      if (treeHeight == sideTree) acc + 1 else if (treeHeight > sideTree) {
+        countVisibleTrees(treeHeight, newCoordinate, direction, forrest, acc + 1)
+      } else acc + 1
+    }
+  }
+
+  private def isVisible(threeHeight: Int, coordinate: Coordinate, direction: Direction, forrest: Forrest): Boolean = {
+    if (forrest.isEdge(coordinate)) true else {
+      val newCoordinate = coordinate.move(direction)
+      threeHeight > forrest.value(newCoordinate) && isVisible(threeHeight, newCoordinate, direction, forrest)
+    }
+  }
+
+  override def part1Answer(): String = {
+    forrest.coordinates.map {
+      coordinate => Direction.all().exists(isVisible(forrest.value(coordinate), coordinate, _, forrest))
+    }.count(_ == true).toString
+  }
+
+  override def part2Answer(): String = {
+    forrest.coordinates.map {
+      coordinate => {
+        val height = forrest.value(coordinate)
+        val scores = Direction.all().map(countVisibleTrees(height, coordinate, _, forrest, 0))
+        scores.filter(_ != 0).product
+      }
+    }.max.toString
+  }
+
+  private case class Forrest(data: Array[Array[Int]]) {
     private val length = data.length - 1
+    val coordinates: Seq[Coordinate] = data.indices.flatMap {
+      index => List(index).zipAll(data.indices, index, index)
+    }.collect {
+      case (x, y) => Coordinate(X(x), Y(y))
+    }
+
     def value(coordinate: Coordinate): Int = data(coordinate.y.value)(coordinate.x.value)
-    def isEdge(coordinate: Coordinate): Boolean = List(0, length).contains(coordinate.x.value) || List(0, length).contains(coordinate.y.value)
+
+    def isEdge(coordinate: Coordinate): Boolean =
+      List(0, length).contains(coordinate.x.value) || List(0, length).contains(coordinate.y.value)
+
   }
 
   case class X(value: Int) {
     def left = X(value - 1)
+
     def right = X(value + 1)
   }
 
   case class Y(value: Int) {
     def top = Y(value - 1)
+
     def bottom = Y(value + 1)
   }
 
@@ -38,6 +83,7 @@ class Day8 extends AoCPuzzle {
   object Direction {
     def all(): List[Direction] = List(Right, Left, Top, Bottom)
   }
+
   sealed trait Direction
 
   object Right extends Direction
@@ -48,32 +94,4 @@ class Day8 extends AoCPuzzle {
 
   object Bottom extends Direction
 
-
-  private def countVisibleTrees(treeHeight: Int, coordinate: Coordinate, direction: Direction, forrest: Forrest, acc: Int): Int = {
-    if (forrest.isEdge(coordinate)) acc else {
-      val newCoordinate = coordinate.move(direction)
-      val sideTree = forrest.value(newCoordinate)
-      if (treeHeight == sideTree) acc + 1 else if (treeHeight > sideTree) {
-        countVisibleTrees(treeHeight, newCoordinate, direction, forrest, (acc + 1))
-      } else acc + 1
-    }
-  }
-
-  override def part1Answer(): String = {
-    val forrest = Forrest(input)
-    var buffer = mutable.ListBuffer[Int]()
-    for (x <- input.indices) {
-      for (y <- input.indices) {
-        val coordinate = Coordinate(X(x), Y(y))
-        val height = forrest.value(coordinate)
-        val scores = Direction.all().map(countVisibleTrees(height, coordinate, _, forrest, 0))
-        val scenicScore = scores.filter( _ != 0).product
-        buffer = buffer.addOne(scenicScore)
-      }
-    }
-    buffer.max.toString
-  }
-
-  override def part2Answer(): String =
-    "???"
 }
